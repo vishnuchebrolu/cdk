dd4923a5dc81f3b34b68a15f275007a890f46b9c
Syed Asad Rahman <s9asad@gmail.com>
2014-03-25 21:49:20 +0000

    import fixed

--- base/isomorphism/src/main/java/org/openscience/cdk/isomorphism/matchers/QueryChemObject.java
+++ base/isomorphism/src/main/java/org/openscience/cdk/isomorphism/matchers/QueryChemObject.java
@@ -27,7 +27,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.openscience.cdk.CDKConstants;
-import org.openscience.cdk.DefaultChemObjectBuilder;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IChemObject;
 import org.openscience.cdk.interfaces.IChemObjectBuilder;
